d98ca6e04be0528fcecaeb6f54970e3c6be8064d
kishor <kishorebsc12@gmail.com>
2025-10-12 13:58:50 +0530

    added

--- db/run.sh
+++ db/run.sh
@@ -1,3 +1,7 @@
-for i in schema app-user master-data; do
-mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < $1.sql
+# for i in schema app-user master-data; do
+# mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < $1.sql
+# done
+
+for i in schema app-user master-data ; do
+  mysql -h ${DB_HOST} -u${DB_USER} -p${DB_PASS} < $i.sql
 done
